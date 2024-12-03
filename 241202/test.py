2e7ba0cfd2a66caf6893b5ea5590da0b8f7aea10
keem02 <contradanza.90@gmail.com>
2024-12-03 09:37:46 +0900

    main

--- 241202/test.py
+++ 241202/test.py
@@ -6,3 +6,15 @@ printnum()
 
 
 
+age = input("enter your age:")
+print(age)
+
+last_name = input("enter your last name: ")
+first_name = input("enter your first name: ")
+
+print("제 이름은 {last name}{first name}입니다")
+# print("제 이름은 {last name}{first name}입니다" .format(last_name,first_name))
+print('4. 제 이름은 %s입니다' 제 나이는 %d입니다.'%(last_name, age))
+      
+
+힘 = 12
