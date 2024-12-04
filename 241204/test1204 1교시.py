fe8cc2cd7e70094e876bbdaf0cd08cacb4969280
keem02 <contradanza.90@gmail.com>
2024-12-04 09:51:49 +0900

    main

---
+++ 241204/test1204 1교시.py
@@ -0,0 +1,30 @@
+# 함수를 만들되 두 가지의 인자를 받고
+# 두 가지의 인자는 각각 문자열이다
+# 두 가지의 인자에서 a라는 갯수를 세고
+# 그 갯수를 조건문 if, else를 사용해서
+# a의 갯수가 더 많으면 True
+# b의 갯수가 더 많으면 False
+# 같으면 -1을 리턴하라
+
+# def compare_a_and_b_count(string1, string2):
+#     # 각 문자열에서 'a'의 갯수 세기
+#     count_a1 = string1.count('a')
+#     count_a2 = string2.count('a')
+    
+#     # 조건에 따라 결과 반환
+#     if count_a1 > count_a2:
+#         return True
+#     elif count_a1 < count_a2:
+#         return False
+#     else:
+#         return -1
+
+# 예제 실행
+result = compare_a_and_b_count("banana", "apple")
+print(result)  # 출력: -1
+
+def strings(apple, banana)
+    
+    count = apple.count(a)
+    count = banana.count(a)
+
