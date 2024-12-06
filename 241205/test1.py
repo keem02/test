15f876f2658c50afa3be88757d6dc9ceaaad678f
keem02 <contradanza.90@gmail.com>
2024-12-06 09:02:09 +0900

    실습

--- 241205/test1.py
+++ 241205/test1.py
@@ -18,25 +18,31 @@
 
 
 
-# a = [x**2 for x in range(1,51) if x % 3 == 0 and x 4 ! = 0]
+# # a = [x**2 for x in range(1,51) if x % 3 == 0 and x 4 ! = 0]
 
 
 
-# 다음과 같은 단어 리스트가 있습니다.
-words = ['hello', 'world', 'python', 'programming', 'language', 'code', 'list', 'data']
+# # 다음과 같은 단어 리스트가 있습니다.
+# words = ['hello', 'world', 'python', 'programming', 'language', 'code', 'list', 'data']
 
-# 아래 조건을 만족하는 새로운 리스트를 만드세요:
-# 1. 단어의 길이가 5글자 초과인 것들만 선택
-# 2. 선택된 단어들 중 'a' 가 포함된 단어들만 선택
-# 3. 최종 선택된 단어들을 대문자로 변환
+# # 아래 조건을 만족하는 새로운 리스트를 만드세요:
+# # 1. 단어의 길이가 5글자 초과인 것들만 선택
+# # 2. 선택된 단어들 중 'a' 가 포함된 단어들만 선택
+# # 3. 최종 선택된 단어들을 대문자로 변환
 
-# 출력 예시: ['LANGUAGE', 'PROGRAMMING']
 
-# li = [word for word in words if len(word) > 5 and ]
+# # 출력 예시: ['LANGUAGE', 'PROGRAMMING']
 
-words = ['hello', 'world', 'python', 'programming', 'language', 'code', 'list', 'data']
+# # li = [word for word in words if len(word) > 5 and ]
 
-li = [word.upper() for word in words 
-      if len(word) > 5 and "a" in word]
+# words = ['hello', 'world', 'python', 'programming', 'language', 'code', 'list', 'data']
 
-print(li)
+# li = [word.upper() for word in words 
+#       if len(word) > 5 and "a" in word]
+
+# print(li)
+
+sum = sum + 1 
+while True:
+    sum = sum + i
+
