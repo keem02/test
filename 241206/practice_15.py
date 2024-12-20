d4eb6ff276b86b79d77dc6b0e6372053a1e21387
keem02 <contradanza.90@gmail.com>
2024-12-20 22:34:23 +0900

    main

--- 241206/practice_15.py
+++ 241206/practice_15.py
@@ -1,2 +1,10 @@
-f = open('python.txt', 'w') # 파일모드 : r, w, a
-f.close()
+# f = open('python.txt', 'w') # 파일모드 : r, w, a
+# f.close()
+
+def print_text():
+    text = "python 기초 배우기"
+    def print()
+        print(text)
+    print()
+
+print_text()
---
+++ 241214/test.py
@@ -0,0 +1,6 @@
+PI = 3.34
+def circle(r,inputpi):
+    z = r*r*inputpi
+    return z
+result = circle(10, PI)
+ 
